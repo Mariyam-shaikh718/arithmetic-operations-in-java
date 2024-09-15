8de5ed129263e3f18642722c0f418b4716bc1640
Mariyam-shaikh718 <mariyamshk56@gmail.com>
2024-09-16 01:21:52 +0500

    Add files via upload

---
+++ SimpleArithmeticOperations.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class SimpleArithmeticOperations {
+    public static void main(String[] args) {
+        // Create a Scanner object for user input
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user to enter the first number
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+
+        // Prompt the user to enter the second number
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        // Perform basic arithmetic operations
+        int addition = num1 + num2;
+        int subtraction = num1 - num2;
+        int multiplication = num1 * num2;
+        int division = num1 / num2;
+        int modulus = num1 % num2;
+
+        // Display the results
+        System.out.println("Addition: " + addition);
+        System.out.println("Subtraction: " + subtraction);
+        System.out.println("Multiplication: " + multiplication);
+        System.out.println("Division: " + division);
+        System.out.println("Modulus: " + modulus);
+
+        // Close the scanner
+        scanner.close();
+    }
+}
